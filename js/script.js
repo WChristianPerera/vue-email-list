d3647ebfa93155fa5dba1c6b16f778fcce1b825f
WChristianPerera <christianperera13@gmail.com>
2023-05-16 17:53:09 +0200

    Create random email generator

--- js/script.js
+++ js/script.js
@@ -7,14 +7,13 @@ const app = Vue.createApp({
 	methods: {
 		getRandomEmail() {
             for(let i = 0; i < 10; i++){
-			axios.get('https://flynn.boolean.careers/exercises/api/random/int')
-				.then(serverResponse => randomEmail.push = serverResponse.data.response);
+			    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(serverResponse => this.randomEmail.push(serverResponse.data.response));
+               
             }
+				       
         },
 
-	},
-	created() {
-		this.getRandomEmail();
 	},
 });
 
